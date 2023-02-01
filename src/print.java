6192ba4ae3e528c11fa4f3eebf15e1b9df165c92
Edog260ttv <118861518+Edog260ttv@users.noreply.github.com>
2023-02-01 11:02:42 -0500

    Create print.java
    new class

---
+++ src/print.java
@@ -0,0 +1,28 @@
+public class print {
+
+
+    public static void printRules() {
+
+        System.out.println("White always goes first. Black goes next, and this repeats for the rest of the game.\n" +
+                "\n" +
+                "To take an enemy piece, you must move a piece onto a square with the enemy piece.\n" +
+                "\n" +
+                "The game is won when one player traps the other player’s king piece, leaving it with no legal moves.\n" +
+                "\n" +
+                "Pawns can move forward one square, except on the first turn, where they can move two forward. Pawns can only attack diagonally forward.\n" +
+                "\n" +
+                "Knights move in an L shape, for example forward 1 tile and down 2 tiles. Knights can move over other pieces.\n" +
+                "\n" +
+                "Bishops can only move diagonally.\n" +
+                "\n" +
+                "Rooks can only move horizontally or vertically.\n" +
+                "\n" +
+                "The queen can move as many squares as she wants diagonally, horizontally, or vertically, making it the most powerful piece on the board.\n" +
+                "\n" +
+                "The king can move any direction, like the queen, but only 1 square. Protect your king.\n" +
+                "\n");
+
+    }
+
+
+}
