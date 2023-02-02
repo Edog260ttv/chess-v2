be10c622860de8f3e4bccccba1674dda3094d0e4
Edog260ttv <118861518+Edog260ttv@users.noreply.github.com>
2023-02-02 12:42:10 -0500

    Update move.java

--- src/move.java
+++ src/move.java
@@ -5,10 +5,13 @@ public class move {
     static String[] blackPieces = new String[6];
 
     static String[][] board = new String[8][8];
+    static String piece = "";
     public static void movePiece(String from,String to) {
+
 
         //Get Board
         board = Main.getBoard();
+
         //king
         whitePieces[0] = "♔";
         //queen
@@ -30,9 +33,11 @@ public class move {
         blackPieces[4] = "♞";
         blackPieces[5] = "♟";
 
-        if (from.substring(0,1).equalsIgnoreCase("a") || from.substring(0,1).equalsIgnoreCase("b") || from.substring(0,1).equalsIgnoreCase("c") || from.substring(0,1).equalsIgnoreCase("d") || from.substring(0,1).equalsIgnoreCase("e") || from.substring(0,1).equalsIgnoreCase("f") || from.substring(0,1).equalsIgnoreCase("g") || from.substring(0,1).equalsIgnoreCase("h")) {
-            int tempY;
-            int tempX;
+
+        // is the coordinate they entered valid?
+        if (((from.substring(0,1).equalsIgnoreCase("a") || from.substring(0,1).equalsIgnoreCase("b") || from.substring(0,1).equalsIgnoreCase("c") || from.substring(0,1).equalsIgnoreCase("d") || from.substring(0,1).equalsIgnoreCase("e") || from.substring(0,1).equalsIgnoreCase("f") || from.substring(0,1).equalsIgnoreCase("g") || from.substring(0,1).equalsIgnoreCase("h")) && (to.substring(0,1).equalsIgnoreCase("a") || to.substring(0,1).equalsIgnoreCase("b") || to.substring(0,1).equalsIgnoreCase("c") || to.substring(0,1).equalsIgnoreCase("d") || to.substring(0,1).equalsIgnoreCase("e") || to.substring(0,1).equalsIgnoreCase("f") || to.substring(0,1).equalsIgnoreCase("g") || to.substring(0,1).equalsIgnoreCase("h"))) && (to.length() < 3 && to.length() > 1 && from.length() < 3 && from.length() > 1) ) {
+            int tempY = 0;
+            int tempX = 0;
             if (from.substring(0,1).equalsIgnoreCase("a")) {
 
                 tempX = 1;
@@ -75,13 +80,67 @@ public class move {
 
             }
 
-            System.out.println();
+
+            int tempY2 = 0;
+            int tempX2 = 0;
+            if (to.substring(0, 1).equalsIgnoreCase("a")) {
+
+                tempX2 = 1;
+                tempY2 = Integer.parseInt(to.substring(1));
+
+            } else if (to.substring(0, 1).equalsIgnoreCase("b")) {
+
+                tempX2 = 2;
+                tempY2 = Integer.parseInt(to.substring(1));
+
+            } else if (to.substring(0, 1).equalsIgnoreCase("c")) {
+
+                tempX2 = 3;
+                tempY2 = Integer.parseInt(to.substring(1));
+
+            } else if (to.substring(0, 1).equalsIgnoreCase("d")) {
+
+                tempX2 = 4;
+                tempY2 = Integer.parseInt(to.substring(1));
+
+               } else if (to.substring(0, 1).equalsIgnoreCase("e")) {
+
+                tempX2 = 5;
+                tempY2 = Integer.parseInt(to.substring(1));
+
+            } else if (to.substring(0, 1).equalsIgnoreCase("f")) {
+
+                tempX2 = 6;
+                tempY2 = Integer.parseInt(to.substring(1));
+
+            } else if (to.substring(0, 1).equalsIgnoreCase("g")) {
+
+                tempX2 = 7;
+                tempY2 = Integer.parseInt(to.substring(1));
+
+            } else if (to.substring(0, 1).equalsIgnoreCase("h")) {
+
+                tempX2 = 8;
+                tempY2 = Integer.parseInt(to.substring(1));
+
+            }
+
+
+                piece = board[tempX][tempY];
+                // coordinate they entered is valid
+                System.out.println("move" + piece +  " from " + tempX + "," + tempY + " to " + tempX2 + "," + tempY2);
+
+
+
+
 
 
 
         } else {
 
-            System.out.println("Please enter a valid Coordinate. the first coordinate you entered is invalid.");
+
+            // coordinated they entered is invalid
+            System.out.println("Please enter a valid Coordinate. one or more of the coordinates you entered is invalid!");
 
         }
 
